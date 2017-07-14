7620c4031dfff23e16a165927aa65b72c6792e69
WolfeTyler <wolfetylerb@gmail.com>
2017-07-14 18:04:05 +0000

    Added article validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ActiveRecord::Base
-    
+    validates :title, presence: true, length: { minimum: 2, maximum: 70}
+    validates :description, presence: true, length: { minimum: 2, maximum: 400}
 end
