267156a1d5be6b532467a389a537e5c5e8ed4aee
WolfeTyler <wolfetylerb@gmail.com>
2017-07-21 19:47:14 +0000

    Added styling to show temp

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
     validates :title, presence: true, length: { minimum: 2, maximum: 70}
-    validates :description, presence: true, length: { minimum: 2, maximum: 400}
+    validates :description, presence: true, length: { minimum: 2, maximum: 4000}
 end
