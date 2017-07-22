1ecad7676256c4bec1e7242fd15be71480deaa4c
WolfeTyler <wolfetylerb@gmail.com>
2017-07-22 21:34:16 +0000

    Add association bt user articles and added debugger

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ActiveRecord::Base
+    
+    belongs_to :user
+    
+    validates :user_id, presence: true
     validates :title, presence: true, length: { minimum: 2, maximum: 70}
     validates :description, presence: true, length: { minimum: 2, maximum: 4000}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ActiveRecord::Base
+
+    has_many :articles
+    before_save { self.email = email.downcase }
+    
     validates :user_name, presence: true, 
                         length: { minimum: 2, maximum: 25}, 
                         uniqueness: { case_sensitive: false}
+    
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    
     validates :email presence: true, 
                         length: { minimum: 5, maximum: 150}, 
                         uniqueness: { case_sensitive: false}, 
