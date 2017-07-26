4b78faf44e5cb8901e21631f0aa6131c2cecdbb8
WolfeTyler <wolfetylerb@gmail.com>
2017-07-26 21:27:48 +0000

    Add Signup Users in UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,7 @@ class User < ActiveRecord::Base
                         length: { minimum: 2, maximum: 25}, 
                         uniqueness: { case_sensitive: false}
     
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    
-    validates :email presence: true, 
-                        length: { minimum: 5, maximum: 150}, 
-                        uniqueness: { case_sensitive: false}, 
-                        format: { with: VALID_EMAIL_REGEX }
+    # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    # validates :email presence: true, length: { minimum: 5, maximum: 150}, 
+    #     uniqueness: { case_sensitive: false}, format: { with: VALID_EMAIL_REGEX }
 end
