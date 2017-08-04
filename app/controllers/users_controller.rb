fe068a22c900266154e7bb4d87e9af20e4f4ba89
WolfeTyler <wolfetylerb@gmail.com>
2017-08-04 03:27:15 +0000

    Added login logout functionality w session authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+    
+    before_action :set_user, only: [:edit, :update, :show]
+    
     def new
         @user = User.new
     end
@@ -13,11 +16,11 @@ class UsersController < ApplicationController
     end
     
     def edit
-        @user = User.find(params[:id])
+        # @user = User.find(params[:id])
     end
     
     def update
-        @user = User.find(params[:id])
+        # @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:success] = "Your account was updated"
             redirect_to articles_path
@@ -27,7 +30,7 @@ class UsersController < ApplicationController
     end
     
     def show
-        @user = User.find(params[:id])
+        # @user = User.find(params[:id])
         @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
     
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,16 @@ Rails.application.routes.draw do
   
   get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
   
   post 'users', to: 'users#create'
+  post 'login', to: 'sessions#create'
+  
+  delete 'logout', to: 'sessions#destroy'
   
   resources :articles
   resources :users
+  
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
