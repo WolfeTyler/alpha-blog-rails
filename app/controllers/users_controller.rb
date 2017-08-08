06023b9a74c1a7cad57cf69ef71f0630ad85c4a7
WolfeTyler <wolfetylerb@gmail.com>
2017-08-08 04:15:00 +0000

    Update users controller user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :email, :password)
+        params.require(:user).permit(:user_name, :email, :password)
     end
     
     def set_user
