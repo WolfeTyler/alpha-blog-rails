8a980d64561b675c28751a1f9d8b6c3d46b21f53
WolfeTyler <wolfetylerb@gmail.com>
2017-08-08 04:07:08 +0000

    Update users create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,25 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+    
+    # def create
+    #     @user = User.new(user_params)
+    #     if @user.save
+    #         flash[:success] = "Welcome #{@user.user_name}"
+    #         redirect_to articles_path
+    #     else
+    #         render 'new'
+    #     end
+    # end
+    
     def create
         @user = User.new(user_params)
         if @user.save
-            flash[:success] = "Welcome #{@user.user_name}"
-            redirect_to articles_path
+          session[:user_id] = @user.id
+          flash[:success] = "Welcome #{@user.user_name}"
+          redirect_to articles_path
         else
-            render 'new'
+          render 'new'
         end
     end
     
