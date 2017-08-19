524298e74a81b712639ad1a79a71bb8494f4a45c
WolfeTyler <wolfetylerb@gmail.com>
2017-08-19 02:48:29 +0000

    Add admin req to category creation

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,6 +4,7 @@ class CategoriesControllerTest < ActionController::TestCase
 
     def setup
         @category = Category.create(name: "sports")
+        @user = User.create( user_name: "testAdmin", email: "testadmin@example.com", password: "password", admin: true)
     end
 
     test "should get the categories index" do
@@ -12,6 +13,7 @@ class CategoriesControllerTest < ActionController::TestCase
     end
 
     test "should get new category" do
+        session[:user_id] = @user.id
         get :new
         assert_response :success
     end
@@ -19,5 +21,12 @@ class CategoriesControllerTest < ActionController::TestCase
     test "should get category show for id" do
         get(:show, {'id' => @category.id})
         assert_response :success
+    end
+    
+    test "should restrict creation to admin" do
+        assert_no_difference 'Category.count' do
+            post :create, category: { name: "Data Structures"}
+        end
+        assert_redirected_to categories_path
     end
 end
